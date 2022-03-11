72f04dc0466078bedd296145db5ebffe37597ae4
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-03-11 13:44:38 +0800

    Create init-ali.sh

---
+++ init-ali.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu
+
+if [[ "$(whoami)" != "root" ]]; then
+	echo "please run this script as root ." >&2
+	exit 1
+fi
+
+set _platform=""
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform        
+    _platform="darwin"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    _platform="linux"
+fi
+
+
+install_kubectl() {
+    ## curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$_platform/amd64/kubectl
+    chmod +x ./kubectl
+    mv ./kubectl /usr/local/bin/kubectl
+    /usr/local/bin/kubectl version --client
+    ## kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+}
+
+install_helm() {
+ 	curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+    helm version --client
+}
+
