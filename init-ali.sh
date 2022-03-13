07ff17ae2ce801d3685db53bc204684046425eb5
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-03-14 03:08:22 +0800

    Update init-ali.sh

--- init-ali.sh
+++ init-ali.sh
@@ -37,7 +37,7 @@ install_git () {
 
 install_kubectl() {
     ## curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$_platform/amd64/kubectl
+    curl -sLO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$_platform/amd64/kubectl
     chmod +x ./kubectl
     mv ./kubectl /usr/local/bin/kubectl
     /usr/local/bin/kubectl version --client
