4e338a2cb0042e388476368bd5ec326aba2bbe08
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-04-09 00:08:52 +0800

    Update ali.sh

--- ali.sh
+++ ali.sh
@@ -18,18 +18,41 @@ function install_git () {
 ## apt -y install git
 ## curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun && systemctl restart docker
 
-function install_docker () {
-  set +e
-  if [[ $(command -v docker >/dev/null; echo $?) -eq 0 ]];
-  then
-    curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun && systemctl restart docker
-  else
-    docker info
-  set -e
+#function install_docker () {
+#  set +e
+#  if [[ $(command -v docker >/dev/null; echo $?) -eq 0 ]];
+#  then
+#    curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun && systemctl restart docker
+#  else
+#    docker info
+#  set -e
+#}
+
+docker_install() {
+    echo "检查Docker......"
+    if [ -x "$(command -v docker)" ]; then
+       echo "检查到Docker已安装!"
+    else
+       if [ -r /etc/os-release ]; then
+            lsb_dist="$(. /etc/os-release && echo "$ID")"
+        fi
+        if [ $lsb_dist == "openwrt" ]; then
+            echo "openwrt 环境请自行安装docker"
+            #exit 1
+        else
+            echo "安装docker环境..."
+            curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+            echo "安装docker环境...安装完成!"
+            systemctl enable docker
+            systemctl start docker
+        fi
+    fi
 }
 
+
 install_git
-install_docker
+
+docker_install
 
 sudo curl -L "http://rancher-mirror.cnrancher.com/docker-compose/v1.27.4/docker-compose-$(uname -s)-$(uname -m)"  \
    -o /usr/local/bin/docker-compose  && \
