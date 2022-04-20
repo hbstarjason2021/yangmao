687459b0f7a67c53f8e25ec87fb826a77975b466
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-04-20 21:43:28 +0800

    Update 02update_carnivalcity.sh

--- 02update_carnivalcity.sh
+++ 02update_carnivalcity.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-## https://gitee.com/starjason/yangmao/raw/main/02update_carnivalcity.sh && bash 02update_carnivalcity.sh
+## wget https://gitee.com/starjason/yangmao/raw/main/02update_carnivalcity.sh && bash 02update_carnivalcity.sh
 
 ## cd -P "$( dirname "$0"  )" && pwd 
 
