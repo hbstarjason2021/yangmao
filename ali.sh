4071d6acb3adebcf91fac1a58b7e6b5ada92b3d0
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-10-27 21:53:21 +0800

    Update ali.sh

--- ali.sh
+++ ali.sh
@@ -50,6 +50,8 @@ install_git
 
 docker_install
 
+# curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 sudo curl -L "http://rancher-mirror.cnrancher.com/docker-compose/v1.27.4/docker-compose-$(uname -s)-$(uname -m)"  \
    -o /usr/local/bin/docker-compose  && \
    sudo chmod +x /usr/local/bin/docker-compose &&  docker-compose version && \
