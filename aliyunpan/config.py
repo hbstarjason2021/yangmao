5caf6c5e90e6193a3273b966951bc493d574ffb6
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-02-17 11:34:39 +0800

    Create config.py

---
+++ aliyunpan/config.py
@@ -0,0 +1,6 @@
+REFRESH_TOKEN_FILE = "./refresh_token.txt"
+ACCESS_TOKEN_FILE = "./access_token.txt"
+EXPIRE_TIME_FILE = "./expire_time.txt"
+ 
+SIGN_LOG = "logs/aliyunpan_sign.log"
+REFRESH_LOG = "logs/aliyunpan_refresh.log"
